45919c7731bab3167e9b0d25df30d932ffbf7456
Andy Smith <andyrsmith@gmail.com>
2014-03-08 22:00:14 +0000

    remove junk

--- src/main/java/com/sockets/server/PongServer.java
+++ src/main/java/com/sockets/server/PongServer.java
@@ -2,11 +2,7 @@ package com.sockets.server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
@@ -14,14 +10,14 @@ import java.nio.channels.SocketChannel;
  * Created by andy on 0w 7/03/14.
  */
 public class PongServer {
-    public static void main(String[] args ) throws IOException {
+    public static void main(String[] args) throws IOException {
         new PongServer().run();
     }
 
     private void run() throws IOException {
         System.out.println("Woohoo");
         InetSocketAddress x = new InetSocketAddress("192.168.1.83", 30000);
-        while(true) {
+        while (true) {
             ServerSocketChannel s = ServerSocketChannel.open();
             s.socket().setReceiveBufferSize(8);
             s.socket().bind(x);
@@ -34,14 +30,15 @@ public class PongServer {
         System.out.println(String.format("Serving %s", sc));
         ByteBuffer bb = ByteBuffer.allocateDirect(8);
         sc.socket().setReceiveBufferSize(8);
-        while(true) {
+        while (true) {
             bb.clear();
             Util.readAll(sc, bb);
             bb.flip();
             long toPing = bb.getLong();
-	    if (toPing % 1000 == 0) {
-		System.out.println("Got " + toPing);
-	    };
+            if (toPing % 1000 == 0) {
+                System.out.println("Got " + toPing);
+            }
+            ;
             bb.clear();
             bb.putLong(toPing);
             bb.flip();
